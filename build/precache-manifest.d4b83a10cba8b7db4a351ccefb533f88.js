652a4bf11a55fdc7da6753a1d15d99b51a1fa772
David <david@MacNoob.local>
2022-12-28 20:11:12 -0500

    update build folder

---
+++ build/precache-manifest.d4b83a10cba8b7db4a351ccefb533f88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f00a7bbb92da72bf5ac",
+    "url": "/mdc-periodic-table/static/css/main.789f3174.chunk.css"
+  },
+  {
+    "revision": "6f00a7bbb92da72bf5ac",
+    "url": "/mdc-periodic-table/static/js/main.6f00a7bb.chunk.js"
+  },
+  {
+    "revision": "9751948b4e23c5016f89",
+    "url": "/mdc-periodic-table/static/js/1.9751948b.chunk.js"
+  },
+  {
+    "revision": "1eb814528f05c9c09a29",
+    "url": "/mdc-periodic-table/static/js/runtime~main.1eb81452.js"
+  },
+  {
+    "revision": "39055678e447eae1a55b71d94846f133",
+    "url": "/mdc-periodic-table/index.html"
+  }
+];
